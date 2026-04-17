a64492227071726a2b0302604acb12550e1c91a6
saurab09876 <sauravsharma09876@gmail.com>
2026-04-17 12:24:32 +0530

    Updated Appsetting.json

--- StudentManagementAPI/obj/Debug/net8.0/StudentManagementAPI.AssemblyInfo.cs
+++ StudentManagementAPI/obj/Debug/net8.0/StudentManagementAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("StudentManagementAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c80d58bd39613e9ea945160067ae1f28596844cb")]
 [assembly: System.Reflection.AssemblyProductAttribute("StudentManagementAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("StudentManagementAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
